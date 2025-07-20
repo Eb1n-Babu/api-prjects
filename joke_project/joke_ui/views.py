89456f4dc903aae0991fa32f848ba0de3feccb8c
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-07-20 17:43:42 +0530

    updated joke-ui

--- joke_project/joke_ui/views.py
+++ joke_project/joke_ui/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def joke_view(request):
+    try:
+        response = request.get("https://icanhazdadjoke.com/",headers={"Accept":"application/json"})
+        response.raise_for_status()
+        joke = response.json().get("joke", "Couldn't find a joke 😢")
+    except:
+        joke = "Oops, no joke today. Blame the internet gremlins 🐛"
+    return render(request, "joke.html", {"joke": joke})
+
